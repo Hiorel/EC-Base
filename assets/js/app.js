e524f4859426cf55cd1a55f66f6b55459ed167ce
Hiorel <s.leclerc076@outlook.fr>
2020-01-10 10:27:09 +0100

    Base + FA + Webpack Encore + SASS

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,41 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// Create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
+require('bootstrap');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+
+function changePlaceholder(change = 1){
+    if(change==1){
+        document.getElementById('search-header').placeholder = 'RECHERCHER...';
+    }else{
+        document.getElementById('search-header').placeholder = '';
+    }
+}
+
+if(window.innerWidth>=370){
+    changePlaceholder(1); 
+}else{
+    changePlaceholder(0);
+}
+
+window.onresize = function(){
+    if(window.innerWidth>=370){
+        changePlaceholder(1); 
+    }else{
+        changePlaceholder(0);
+    }
+}
